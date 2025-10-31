c3bed30e6ce722db4261c030765f5100ab5e3221
mohammadtwo <bentenison61@gmail.com>
2025-10-31 21:40:47 +0330

    feat:H1 & scrolbar:chenge style & chenge bg

--- src/main.js
+++ src/main.js
@@ -99,16 +99,18 @@ let rightToService = document.getElementById("right_to_service");
 let DiscountInput = document.getElementById("Discount");
 let DiscountBtn = document.getElementById("DiscountBtn");
 let discount = document.getElementById("discount");
-  rightToService.innerText = 0;
-    discount.innerText = 0;
+rightToService.innerText = 0;
+discount.innerText = 0;
 
-  final.innerText = 0;
-let finalPrice =document.getElementById("total-price1")
-function finalPrice1(){
-  finalPrice.innerText=''
-  finalPrice.innerText = Number(final.innerText) + Number( rightToService.innerText) -Number( discount.innerText);
+final.innerText = 0;
+let finalPrice = document.getElementById("total-price1");
+function finalPrice1() {
+  finalPrice.innerText = "";
+  finalPrice.innerText =
+    Number(final.innerText) +
+    Number(rightToService.innerText) -
+    Number(discount.innerText);
 }
-
 
 function sum() {
   rightToService.innerText = "";
@@ -121,52 +123,52 @@ function sum() {
   rightToService.innerText = result * 0.0009;
   discount.innerText = 0;
 
-finalPrice1();
-
+  finalPrice1();
 }
-function Discount(DiscountInput, final,rightToService) {
+function Discount(DiscountInput, final, rightToService) {
   let DiscountV = DiscountInput.value.toLowerCase();
-  let result = Number(final.innerText) + Number(rightToService.innerText)
+  let result = Number(final.innerText) + Number(rightToService.innerText);
   switch (DiscountV) {
     case "gold":
       result *= 0.2;
       break;
-      case "silver":
-        result *= 0.15;
+    case "silver":
+      result *= 0.15;
       break;
     case "bronze":
       result *= 0.1;
       break;
-      default:
-        result = "کد تخفیف وارد شده صحیح نمیباشد";
-        break;
-      }
-      DiscountV=''
-      DiscountInput.value=''
-      discount.innerText=''
-      discount.innerText = result;
-finalPrice1();
-
+    default:
+      result = "کد تخفیف وارد شده صحیح نمیباشد";
+      break;
+  }
+  DiscountV = "";
+  DiscountInput.value = "";
+  discount.innerText = "";
+  discount.innerText = result;
+  finalPrice1();
 }
-DiscountBtn.addEventListener("click",()=>{
-Discount(DiscountInput, final, rightToService);
-
-})
+DiscountBtn.addEventListener("click", () => {
+  Discount(DiscountInput, final, rightToService);
+});
 const counter = document.getElementById("card_cuntiner");
 counter.innerHTML = "";
 
 function showItem() {
-  discount.innerText=0
-
-  let result = data.products
-    .map((item) => {
+  discount.innerText = 0;
+  counter.innerHTML = `
+      <h1 class='absolute w-full flex justify-center items-center to-0 right- pt-5 text-4xl text-white' >رستوران مک دونالد شعبه تهران</h1>
+    <div class=" fixed w-full h-4 bg-[#00688e] bottom-0 right-0"></div>
+  `;
+ let result = data.products
+    .map((item) => { 
       return `<div
           
-          class="card grid grid-cols-4 grid-rows-3 bg-white m-auto mt-5 max-h-[150px] w-[95%] h-[150px] gap-x-2.5 overflow-hidden rounded-2xl"
+          class="card grid grid-cols-4 grid-rows-3 bg-white m-auto my-2 max-h-[150px] w-[95%] h-[150px] gap-x-2.5 overflow-hidden rounded-2xl"
         >
           <div class=" col-start-1 col-end-3 row-start-1 row-end-4 relative">
             <img
-              class="object-cover w-full h-full"
+              class="object-cover w-full max-w-[170px] h-full"
               src="${item.img}"
               alt="Berguer"
             />
@@ -177,31 +179,31 @@ function showItem() {
             <span>تومان</span><span class="price" >${item.price}</span>
           </p>
           <div
-            class="max1 min-w-[50px] border-2 rounded-[9px]  h-6  flex items-center justify-center col-start-3 col-end-4 row-start-3 row-end-4"
+            class="max1 min-w-[50px] overflow-hidden rounded-[9px]  h-6  flex items-center justify-center col-start-3 col-end-4 row-start-3 row-end-4"
           >
             <button
-              class="plus text-center text-lg w-full text-black h-full flex items-center justify-center " 
+              class="plus text-center text-lg w-full bg-[#008191] text-black h-full flex items-center justify-center " 
             >
               +
             </button>
             <p 
-              class="counter text-lg text-center  flex items-center justify-center"
+              class="counter text-lg lg:mx-3 mx-1 text-center  flex items-center justify-center"
             >
               0
             </p>
             <button
-              class="mines text-center text-lg w-full h-full flex items-center justify-center"
+              class="mines bg-[#008191] text-center text-lg w-full h-full flex items-center justify-center"
             >
               -
             </button>
           </div>
-          <div class="col-start-4 col-end-5 row-start-3 row-end-4 ml-5 mr-auto">
-            <span class="total">0</span><span>تومان</span>
+          <div class="col-start-4 col-end-5 row-start-3 row-end-4 ml-5 mr-auto flex flex-wrap">
+            <span class="total"></span><span>تومان</span>
           </div>
         </div>`;
     })
     .join("");
-  counter.innerHTML = result;
+  counter.innerHTML += result;
 }
 showItem();
 totalPrice();
@@ -219,9 +221,7 @@ function totalPrice() {
       Cunts[index].innerText = val;
       total[index].innerText = "";
       total[index].innerText = parseInt(val * price[index].innerText);
-      console.log(total[index].innerText);
       data.products[index].total = +total[index].innerText;
-      console.log(data.products);
       sum();
     })
   );
@@ -241,19 +241,18 @@ function totalPrice() {
 }
 finalPrice1();
 const submitOrder = document.getElementById("submitOrder");
-   submitOrder.addEventListener("click", () => {
-     data.products.forEach((prod) => {
-       prod.total = 0;
-     });
+submitOrder.addEventListener("click", () => {
+  data.products.forEach((prod) => {
+    prod.total = 0;
+  });
 
-     final.innerText = 0;
-     rightToService.innerText = 0;
-     discount.innerText = 0;
-     DiscountInput.value = "";
+  final.innerText = 0;
+  rightToService.innerText = 0;
+  discount.innerText = 0;
+  DiscountInput.value = "";
   finalPrice.innerText = 0;
 
-     showItem();
-     totalPrice();
-     alert("سفارش با موفقیت ثبت شد!");
-
-   });
+  showItem();
+  totalPrice();
+  alert("سفارش با موفقیت ثبت شد!");
+});
